f5bbd1b00d9b11fb23b10b6dc2589adb50a39ca9
osman hamad <osmansays@gmail.com>
2019-09-09 13:48:03 +0300

    selenuim push

---
+++ testEdurekaPhpWebsite/Program.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace testEdurekaPhpWebsite
+{
+    class Program
+    {
+       // public static IWebDriver driver;
+
+        public static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(@"D:\ChromeDriver\chromedriver_win32\");
+            driver.Url = "http://192.168.36.200:8000/index.php";
+
+            
+            IWebElement AboutUsLink= driver.FindElement(By.LinkText("About Us"));
+            AboutUsLink.Click();
+              if (driver.PageSource.Contains("<b>about</b>"))
+                Console.WriteLine("About us page is working fine");
+            else
+                Console.WriteLine("There is a problem on about us page");
+            driver.Close();
+        }
+   
+
+    }
+}
---
+++ testEdurekaPhpWebsite/TestPhpWebsite.cs
@@ -0,0 +1,36 @@
+﻿using NUnit;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace testEdurekaPhpWebsite
+{
+    class TestPhpWebsite
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestPage()
+        {
+            driver.Url = "http://www.google.com";
+
+        }
+
+        [TearDown]
+        public void Closebroswer ()
+        {
+            driver.Close();
+
+        }
+
+    }
+}
