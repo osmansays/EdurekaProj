eb6f13b99346987182228fd7925bb1364a42f243
osman hamad <osmansays@gmail.com>
2019-10-01 14:13:34 +0300

    addint test

--- NUnitTestPhpWebsite/UnitTest1.cs
+++ NUnitTestPhpWebsite/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Runtime.InteropServices;
 
 namespace Tests
 {
@@ -9,7 +10,22 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
-            IWebDriver driver = new ChromeDriver(); //(@"D:\ChromeDriver\chromedriver_win32\");
+
+            //ChromeOptions options = new ChromeOptions();
+            //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+            //options.AddArguments("start-maximized"); // open Browser in maximized mode
+            //options.AddArguments("disable-infobars"); // disabling infobars
+            //options.AddArguments("--disable-extensions"); // disabling extensions
+            ////options.AddArguments("--disable-gpu"); // applicable to windows os only
+            //options.AddArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+            //options.AddArguments("--no-sandbox"); // Bypass OS security model
+            IWebDriver driver=new ChromeDriver();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+             driver = new ChromeDriver(@"/usr/bin/chromedriver"); //(@"D:\ChromeDriver\chromedriver_win32\");
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                driver = new ChromeDriver(@"chromedriver");
+
             driver.Url = "http://192.168.37.21/index.php";
 
             IWebElement AboutUsLink = driver.FindElement(By.LinkText("About Us"));
