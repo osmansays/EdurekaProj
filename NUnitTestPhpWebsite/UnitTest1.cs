63b54ebb99b9f8110e648a0fcca9ee4cdbe523f3
osman hamad <osmansays@gmail.com>
2019-10-02 10:35:23 +0300

    relocating ansible yaml files

--- NUnitTestPhpWebsite/UnitTest1.cs
+++ NUnitTestPhpWebsite/UnitTest1.cs
@@ -13,22 +13,13 @@ namespace Tests
         {
 
             ChromeOptions options = new ChromeOptions();
-            //setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-            options.AddArguments("start-maximized"); // open Browser in maximized mode
-            options.AddArguments("disable-infobars"); // disabling infobars
-            options.AddArguments("--disable-extensions"); // disabling extensions
-            //options.AddArguments("--disable-gpu"); // applicable to windows os only
-            options.AddArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-            options.AddArguments("--no-sandbox"); // Bypass OS security model
-            options.AddArguments("--disable-setuid-sandbox");            //IWebDriver driver=new ChromeDriver();
+            options.AddArguments("start-maximized"); 
+            options.AddArguments("disable-infobars"); 
+            options.AddArguments("--disable-extensions"); 
+            options.AddArguments("--disable-dev-shm-usage"); 
+            options.AddArguments("--no-sandbox"); 
+            options.AddArguments("--disable-setuid-sandbox");
             options.AddArguments("headless");
-            //  if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            //IWebDriver driver = new ChromeDriver(@"/usr/bin/",options); //
-           // IWebDriver driver = new ChromeDriver(@"D:\ChromeDriver\chromedriver_win32\");
-
-            //   if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                  //driver = new ChromeDriver(@"chromedriver");
-
             string driverPath = "/usr/bin/";
             string driverExecutableFileName = "chromedriver";
             
@@ -42,11 +33,9 @@ namespace Tests
             if (driver.PageSource.Contains("<b>about</b>"))
             {
                 Assert.Pass();
-                //   Console.WriteLine("About us page is working fine");
             }
             else
                 Assert.Fail();
-                //Console.WriteLine("There is a problem on about us page");
             driver.Close();
             
 
