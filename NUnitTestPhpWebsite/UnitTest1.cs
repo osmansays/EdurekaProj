9c0b92b54ff665ef3a3dac286717b096fe2ae279
osman hamad <osmansays@gmail.com>
2019-10-02 08:30:07 +0300

    addint test

--- NUnitTestPhpWebsite/UnitTest1.cs
+++ NUnitTestPhpWebsite/UnitTest1.cs
@@ -11,20 +11,20 @@ namespace Tests
         public void Setup()
         {
 
-            //ChromeOptions options = new ChromeOptions();
+            ChromeOptions options = new ChromeOptions();
             //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-            //options.AddArguments("start-maximized"); // open Browser in maximized mode
-            //options.AddArguments("disable-infobars"); // disabling infobars
-            //options.AddArguments("--disable-extensions"); // disabling extensions
-            ////options.AddArguments("--disable-gpu"); // applicable to windows os only
-            //options.AddArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-            //options.AddArguments("--no-sandbox"); // Bypass OS security model
+            options.AddArguments("start-maximized"); // open Browser in maximized mode
+            options.AddArguments("disable-infobars"); // disabling infobars
+            options.AddArguments("--disable-extensions"); // disabling extensions
+            //options.AddArguments("--disable-gpu"); // applicable to windows os only
+            options.AddArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+            options.AddArguments("--no-sandbox"); // Bypass OS security model
             IWebDriver driver=new ChromeDriver();
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-             driver = new ChromeDriver(@"/usr/bin/chromedriver"); //(@"D:\ChromeDriver\chromedriver_win32\");
+          //  if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+             driver = new ChromeDriver(@"/usr/bin/chromedriver",options); //(@"D:\ChromeDriver\chromedriver_win32\");
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                driver = new ChromeDriver(@"chromedriver");
+         //   if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+          //      driver = new ChromeDriver(@"chromedriver");
 
             driver.Url = "http://192.168.37.21/index.php";
 
